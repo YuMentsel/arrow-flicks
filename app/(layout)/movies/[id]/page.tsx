1f1d189b1152eb3e675a83ad840437c00ca3f2ab
Yu Mentsel <yu.mentsel@gmail.com>
2024-05-25 01:39:05 +0300

    feat: add breadcrumbs

--- app/(layout)/movies/[id]/page.tsx
+++ app/(layout)/movies/[id]/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { Stack } from '@mantine/core';
+import Link from 'next/link';
+import { Breadcrumbs, Stack } from '@mantine/core';
 import LoaderDots from '@/app/components/LoaderDots';
 import MovieDetails from './components/MovieDetails';
 import { useMovie } from '@/app/lib/hooks/useMoviesDataHooks';
+import { Path } from '@/app/types/enums';
+import classes from './styles.module.css';
 
 interface MoviePageProps {
   params: { id: string };
@@ -19,6 +22,10 @@ export default function MoviePage({ params: { id } }: Readonly<MoviePageProps>) 
   return (
     data && (
       <Stack w="100%" maw="50rem" mx="auto">
+        <Breadcrumbs classNames={classes} td="none" h="1.88rem" mt="xs">
+          <Link href="/">Movies</Link>
+          <Link href={`/${Path.Movies}/${data.id}`}>{data.title}</Link>
+        </Breadcrumbs>
         <MovieDetails movie={data} />
         {/* <Trailer movie={data} /> */}
       </Stack>
